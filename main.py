135c48fc1b7f41fb205fdf90db0e23cceb079d9d
Ivan <divan22018@gmail.com>
2022-08-05 11:52:27 +0300

    ADc and PWM

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import  time
 
 pwm0 = PWM(Pin(21))
 #p1 = Pin(4, Pin.IN, Pin.PULL_UP)
-adc = ADC(Pin(22))
+adc = ADC(Pin(33))
 while True:
     val = adc.read_uv()
     print(val)
-    pwm0.duty(val/1000)
+    pwm0.duty(int(val/2000))
 #    time.sleep_ms(1000)
